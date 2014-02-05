d6d8e33b19e8e95d30cc25c39389b77ca9335f3a
LeeW <cl87654321@gmail.com>
2014-02-05 16:41:15 +0800

    POJ1002 TLE

--- HW1/POJ1002/POJ1002.cpp
+++ HW1/POJ1002/POJ1002.cpp
@@ -1,6 +1,6 @@
-// current state :
+// current state : TLE
 // time (first submit) : 45:44
-// state (first submit) : CE ...
+// state (first submit) : CE -> TLE
 // error reason :
 
 // #include <cstdio>
@@ -11,11 +11,7 @@
 using namespace std;
 
 void turnToStandardFrom(string&);
-void printElement(pair<int,string>);
-
-void reverseDuplicate(pair<string,int> p,map<int, string>&m) {
-    m[p.second] = p.first;
-}
+void printElement(pair<string,int>);
 
 main()
 {
@@ -31,11 +27,7 @@ main()
         numOfEachTel[tel]++;
     }
 
-    map<int,string>tmpMap;
-    // for_each(numOfEachTel.begin(),numOfEachTel.end(),bind2nd(ptr_fun(reverseDuplicate),tmpMap));
-
-
-    // for_each(tmpMap.begin(), tmpMap.end(),printElement);
+    for_each(numOfEachTel.map<string,int>::lower_bound(1),numOfEachTel.end(),printElement);
 }
 
 void turnToStandardFrom(string& tel) {
@@ -49,46 +41,28 @@ void turnToStandardFrom(string& tel) {
             continue;
         }
 
-        if ('A'<=tel[i] && tel[i]<='C')
-            standardTel += '2';
-        else if ('D'<=tel[i] && tel[i]<='F')
-            standardTel += '3';
-        else if ('G'<=tel[i] && tel[i]<='I')
-            standardTel += '4';
-        else if ('J'<=tel[i] && tel[i]<='L')
-            standardTel += '5';
-        else if ('M'<=tel[i] && tel[i]<='O')
-            standardTel += '6';
-        else if ('P'<=tel[i] && tel[i]<='S' && tel[i] != 'Q')
-            standardTel += '7';
-        else if ('T'<=tel[i] && tel[i]<='V')
-            standardTel += '8';
-        else if ('W'<=tel[i] && tel[i]<='Y')
-            standardTel += '9';
+//
+//         if ('A'<=tel[i] && tel[i]<='C')
+//             standardTel += '2';
+//         else if ('D'<=tel[i] && tel[i]<='F')
+//             standardTel += '3';
+//         else if ('G'<=tel[i] && tel[i]<='I')
+//             standardTel += '4';
+//         else if ('J'<=tel[i] && tel[i]<='L')
+//             standardTel += '5';
+//         else if ('M'<=tel[i] && tel[i]<='O')
+//             standardTel += '6';
+//         else if ('P'<=tel[i] && tel[i]<='S' && tel[i] != 'Q')
+//             standardTel += '7';
+//         else if ('T'<=tel[i] && tel[i]<='V')
+//             standardTel += '8';
+//         else if ('W'<=tel[i] && tel[i]<='Y')
+//             standardTel += '9';
     }
     tel = standardTel;
 }
 
-void printElement(pair<int,string>p) {
-    if (p.first > 1)
-        cout << p.second << " " << p.first<<endl;
+void printElement(pair<string,int>p) {
+    if (p.second > 1)
+        cout << p.first << " " << p.second<<endl;
 }
-//
-// Compile Error
-//
-// In file included from stl_function.h:712,
-//                  from string:50,
-//                  from locale_classes.h:42,
-//                  from ios_base.h:43,
-//                  from ios:43,
-//                  from ostream:40,
-//                  from iostream:40,
-//                  from F:\temp\12497237.92535\Main.cc:6:
-// binders.h: In instantiation of 'std::binder2nd<std::pointer_to_binary_function<std::pair<std::basic_string<char, std::char_traits<char>, std::allocator<char> >, int>, std::map<int, std::basic_string<char, std::char_traits<char>, std::allocator<char> >, std::less<int>, std::allocator<std::pair<const int, std::basic_string<char, std::char_traits<char>, std::allocator<char> > > > >&, void> >':
-// F:\temp\12497237.92535\Main.cc:34:   instantiated from here
-// binders.h:141: error: forming reference to reference type 'std::map<int, std::basic_string<char, std::char_traits<char>, std::allocator<char> >, std::less<int>, std::allocator<std::pair<const int, std::basic_string<char, std::char_traits<char>, std::allocator<char> > > > >&'
-// binders.h: In function 'std::binder2nd<_Operation> std::bind2nd(const _Operation&, const _Tp&) [with _Operation = std::pointer_to_binary_function<std::pair<std::basic_string<char, std::char_traits<char>, std::allocator<char> >, int>, std::map<int, std::basic_string<char, std::char_traits<char>, std::allocator<char> >, std::less<int>, std::allocator<std::pair<const int, std::basic_string<char, std::char_traits<char>, std::allocator<char> > > > >&, void>, _Tp = std::map<int, std::basic_string<char, std::char_traits<char>, std::allocator<char> >, std::less<int>, std::allocator<std::pair<const int, std::basic_string<char, std::char_traits<char>, std::allocator<char> > > > >]':
-// F:\temp\12497237.92535\Main.cc:34:   instantiated from here
-// binders.h:162: error: no matching function for call to 'std::binder2nd<std::pointer_to_binary_function<std::pair<std::basic_string<char, std::char_traits<char>, std::allocator<char> >, int>, std::map<int, std::basic_string<char, std::char_traits<char>, std::allocator<char> >, std::less<int>, std::allocator<std::pair<const int, std::basic_string<char, std::char_traits<char>, std::allocator<char> > > > >&, void> >::binder2nd(const std::pointer_to_binary_function<std::pair<std::basic_string<char, std::char_traits<char>, std::allocator<char> >, int>, std::map<int, std::basic_string<char, std::char_traits<char>, std::allocator<char> >, std::less<int>, std::allocator<std::pair<const int, std::basic_string<char, std::char_traits<char>, std::allocator<char> > > > >&, void>&, std::map<int, std::basic_string<char, std::char_traits<char>, std::allocator<char> >, std::less<int>, std::allocator<std::pair<const int, std::basic_string<char, std::char_traits<char>, std::allocator<char> > > > >&)'
-// binders.h:135: note: candidates are: std::binder2nd<std::pointer_to_binary_function<std::pair<std::basic_string<char, std::char_traits<char>, std::allocator<char> >, int>, std::map<int, std::basic_string<char, std::char_traits<char>, std::allocator<char> >, std::less<int>, std::allocator<std::pair<const int, std::basic_string<char, std::char_traits<char>, std::allocator<char> > > > >&, void> >::binder2nd(const std::binder2nd<std::pointer_to_binary_function<std::pair<std::basic_string<char, std::char_traits<char>, std::allocator<char> >, int>, std::map<int, std::basic_string<char, std::char_traits<char>, std::allocator<char> >, std::less<int>, std::allocator<std::pair<const int, std::basic_string<char, std::char_traits<char>, std::allocator<char> > > > >&, void> >&)
-//  
