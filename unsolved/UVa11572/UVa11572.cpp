50cd5ce090697bb50daba669fee1c3c4d727f82f
LeeW <cl87654321@gmail.com>
2014-02-15 18:37:07 +0800

    slightly change Uva11572 but still not solve

--- unsolved/UVa11572/UVa11572.cpp
+++ unsolved/UVa11572/UVa11572.cpp
@@ -1,25 +1,53 @@
 // current state :
 // time (first submit) : 12:30
-// state (first submit) :
-// error reason :
+// state (first submit) : WA
+// error reason : totally misunderstand what the problem said
 
 #include <cstdio>
 #include <map>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 main() {
+    int caseNum, snowFlakeNum, snowFlakeID;
+    vector<int> snowFlakes;
 
-    map<int,bool>packages;
-    int castNum, snowFlakeNum, snowFlakeID;
-
-    scanf("%d", &castNum);
-    for (int CASE = 0; CASE < castNum; CASE++) {
+    scanf("%d", &caseNum);
+    while(caseNum--) {
         scanf("%d", &snowFlakeNum);
+        int maxPackageSize = 0;
+        map<int, int> package;
+
         for (int i = 0; i < snowFlakeNum; i++) {
             scanf("%d", &snowFlakeID);
-            packages[snowFlakeID] = true;
+            snowFlakes.push_back(snowFlakeID);
         }
-        printf("%ld\n",packages.size());
-        packages.clear();
+
+        for (int i = 0; i < snowFlakeNum; i++) {
+            if (!package[snowFlakes[i]])
+                package[snowFlakes[i]] = 1;
+            else {
+                maxPackageSize = max(maxPackageSize, (int)package.size());
+                package.clear();
+                package[snowFlakes[i]] = 1;
+            }
+        }
+        maxPackageSize = max(maxPackageSize, (int)package.size());
+        package.clear();
+
+        // for (int i = snowFlakeNum-1; i >=0; i--) {
+        //     if (!package[snowFlakes[i]])
+        //         package[snowFlakes[i]] = 1;
+        //     else {
+        //         maxPackageSize = max(maxPackageSize, (int)package.size());
+        //         package.clear();
+        //         package[snowFlakes[i]] = 1;
+        //     }
+        // }
+        // maxPackageSize = max(maxPackageSize, (int)package.size());
+        // package.clear();
+        //
+        printf("%d\n", maxPackageSize);
     }
 }
