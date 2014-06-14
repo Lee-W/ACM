409c97c0d2477bc4e61b942be9c0eb9f14ef42b9
LeeW <cl87654321@gmail.com>
2014-06-15 00:52:25 +0800

    add POJ 1269

--- POJ/1269/1269.cpp
+++ POJ/1269/1269.cpp
@@ -1,8 +1,10 @@
 #include <cstdio>
 #include <cmath>
+#include <climits>
 
 #define pointNum 4
 #define lineNum 2
+#define INF INT_MAX/10
 #define eps 1e-8
 
 struct Point {
@@ -14,18 +16,30 @@ struct Line {   // y = ax + b
     double a, b;
 
     Line (Point p1, Point p2) {
-        a = (p1.y - p2.y) / (p1.x - p2.x);
-        b = a*p1.x - p1.y;
-        a = nan;
+        if (p1.x - p2.x) {
+            a = (p1.y - p2.y) / (p1.x - p2.x);
+            b = p1.y - a*p1.x;
+        } else {
+            a = INF;
+            b = -p1.x;
+        }
     }
+
     Point getIntersectionPoint(Line li) {
         double x, y;
-        x = (li.b - this->b) / (this->a - li.a) + eps;
-        y = this->a * x + this->b + eps;
+        if (this->a == INF) {
+            x = -this->b;
+            y = li.a * x + li.b;
+        } else if (li.a == INF){
+            x = -li.b;
+            y = this->a * x + this->b;
+        } else {
+            x = (li.b - this->b) / (this->a - li.a) + eps;
+            y = this->a * x + this->b + eps;
+        }
         return Point(x, y);
     }
 };
-
 
 Point p[pointNum];
 
@@ -41,7 +55,6 @@ int main()
 
         Line line1(p[0], p[1]), line2(p[2], p[3]);
 
-//        printf ("%lf %lf %lf %lf\n", line1.a, line1.b, line2.a, line2.b);
         if (std::abs(line1.a - line2.a) < eps) {
             if (std::abs(line1.b - line2.b) < eps)
                 printf("LINE\n");
@@ -49,9 +62,8 @@ int main()
                 printf("NONE\n");
         } else {
             Point intersectionPoint = line1.getIntersectionPoint(line2);
-            printf("POINT %.2lf %.2lf\n", -intersectionPoint.x, -intersectionPoint.y);
+            printf("POINT %.2f %.2f\n", intersectionPoint.x, intersectionPoint.y);
         }
     }
     printf("END OF OUTPUT\n");
-
 }
